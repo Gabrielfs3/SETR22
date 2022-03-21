1cbb51eb236c3db7f3aff56391c6ad386dc3f66f
Luis Malarmey <lmalarmey@ua.pt>
2022-03-21 11:22:49 +0000

    parte do Doxy para vinit.c

--- Trabalho1/vinit.c
+++ Trabalho1/vinit.c
@@ -1,3 +1,13 @@
+/**
+ * @file vinit.c
+ * @author Luis Malarmey (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 /**
