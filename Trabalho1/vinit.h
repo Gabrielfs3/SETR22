905ad6bf344fa0990772c326c6dea118d31216df
Gabrielfs3 <gabrielfs@ua.pt>
2022-03-21 11:52:49 +0000

    update

--- Trabalho1/vinit.h
+++ Trabalho1/vinit.h
@@ -1,16 +1,1 @@
 int vinit (int vect[],int size);
-
-/**
- * @file vinit.c
- * @author Luis Malarmey (you@domain.com)
- * @brief Função que preenche o array
- * 
- * @param vect Array de inteiros
- * @param size Tamanho do array
- * @return vect[size] 
- * @version 0.1
- * @date 2022-03-21
- * 
- * @copyright Copyright (c) 2022
- * 
- */
