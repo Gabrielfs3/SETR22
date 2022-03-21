ef949a55e696a37e9a3862e44b86f5979b868f74
Luis Malarmey <lmalarmey@ua.pt>
2022-03-21 11:18:44 +0000

    parte do Doxy para vinit.c

--- Trabalho1/vinit.c
+++ Trabalho1/vinit.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * @brief Função que preenche o array
+ * 
+ * @param vect Array de inteiros
+ * @param size Tamanho do array
+ * @return vect[size] 
+ */
 int vinit (int vect[],int size)
 {
     static int cnt = 1;
