8090a1f9a33bae5758273f64b7870621f201ccd8
Gabrielfs3 <gabrielfs@ua.pt>
2022-03-22 14:38:33 +0000

    Initial comitt

---
+++ Assigment1/MySAG.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "MySAGInit.h"
+#include "MySAGInsert.h"
+#include "MySAGMax.h"
+#include "MySAGMin.h"
+#include "MySAGAvg.h"
+#include "MySAGFreq.h"
+
+
+int main()
+{
+
+    return 0;
+}
